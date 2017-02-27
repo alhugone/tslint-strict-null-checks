ffe6b92a5bb3d6057d0f7a02f5d27bb1f8972eb5
Hugo <alhugone@gmail.com>
2017-02-27 22:26:18 +0100

    Refactor

--- test/noUnitialized.spec.ts
+++ test/noUnitialized.spec.ts
@@ -4,13 +4,13 @@ import * as variableInit from './noUnitialized.variableInitTestCases';
 describe('no-uninitialized-non-undefined-var', () => {
     variableInit.testCases.forEach(function (test) {
         it(`when variable of non-undefined type is not initialized when declared, should return failure: ${test.source}`, () => {
-            let result: tslint.LintResult = lint(test.source);
+            let result: tslint.LintResult = lint(test.source, ['variable']);
             expect(result.failureCount !== 0).toBe(test.shouldWarn, 'Incorrect lint result');
         });
     });
 });
 
-function lint(source: string): tslint.LintResult {
+function lint(source: string, options: string[]): tslint.LintResult {
     let linterOptions: tslint.ILinterOptions = {
         fix: false,
         formatter: 'json',
@@ -19,7 +19,7 @@ function lint(source: string): tslint.LintResult {
     };
     let configuration = {
         rules: {
-            'no-uninitialized': [true, 'variable']
+            'no-uninitialized': [true, ...options]
         },
     };
     let linter = new tslint.Linter(linterOptions, undefined);
