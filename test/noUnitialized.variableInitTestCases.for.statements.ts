509d237be19df3095b287d50e56d3460fc272388
Hugo <alhugone@gmail.com>
2017-05-08 22:08:16 +0200

    .npmignore adedd

--- test/noUnitialized.variableInitTestCases.for.statements.ts
+++ test/noUnitialized.variableInitTestCases.for.statements.ts
@@ -6,4 +6,17 @@ export const testCases = [
             console.log(val);
         }`, 
     },
+    { shouldWarn: false,
+      source: `
+        const my_list = [5, 6, 7];
+        for (const val in my_list) {
+            console.log(val);
+        }`, 
+    },
+    { shouldWarn: false,
+      source: `
+        for (var val:string, k = 1; k<10;k++) {
+            console.log(val);
+        }`, 
+    },
 ];
