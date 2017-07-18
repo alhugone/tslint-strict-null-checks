45730a31f3392ec3109ddaf7412d535d8a07858a
Hugo <alhugone@gmail.com>
2017-07-18 22:08:26 +0200

    Update code to new TSLint version

--- test/noUnitialized.spec.ts
+++ test/noUnitialized.spec.ts
@@ -9,14 +9,14 @@ describe('no-uninitialized variables', () => {
     variableInit.testCases.forEach(function (test) {
         it(`when declared variable of non-undefined type is not explicitly initialized, should return failure: ${test.source}`, () => {
             let result: tslint.LintResult = lint(test.source, ['variables']);
-            expect(result.failureCount !== 0).toBe(test.shouldWarn, 'Incorrect lint result');
+            expect(result.errorCount !== 0).toBe(test.shouldWarn, 'Incorrect lint result');
         });
     });
 
     variableInitInForStatement.testCases.forEach(function (test) {
         it(`when declared variable of non-undefined type is used in for statement, should return failure: ${test.source}`, () => {
             let result: tslint.LintResult = lint(test.source, ['variables']);
-            expect(result.failureCount !== 0).toBe(test.shouldWarn, 'Incorrect lint result');
+            expect(result.errorCount !== 0).toBe(test.shouldWarn, 'Incorrect lint result');
         });
     });
 });
@@ -26,7 +26,7 @@ describe('no-uninitialized properties', () => {
     propertyInit.testCases.forEach(function (test) {
         it(`when class property of non-undefined type is not explicitly initialized, should return failure: ${test.source}`, () => {
             let result: tslint.LintResult = lint(test.source, ['properties']);
-            expect(result.failureCount !== 0).toBe(test.shouldWarn, 'Incorrect lint result');
+            expect(result.errorCount !== 0).toBe(test.shouldWarn, 'Incorrect lint result');
         });
     });
 });
